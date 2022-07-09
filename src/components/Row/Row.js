ceff06348d49e2c012d155197f524a459a00cb2f
Harshit <sahilchopra838@gmail.com>
2022-07-09 20:43:27 +0530

    added triler functionality

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "./Row.css";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movie, setMovie] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -15,6 +18,27 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     const request = await axios.get(fetchUrl);
     setMovie(request.data.results);
   }
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_title || "")
+        .then((url) => {
+          console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   return (
     <div className="row">
@@ -25,6 +49,7 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             <img
               className={`row_poster ${isLargeRow && "rowPoster_large"}`}
               key={movie.id}
+              onClick={() => handleClick(movie)}
               src={`${base_url}${
                 isLargeRow ? movie?.poster_path : movie?.backdrop_path
               }`}
@@ -35,6 +60,7 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
           )
         )}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
